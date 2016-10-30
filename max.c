53fc7c7cbd7a0e0dca046427618480af5bb08c2b
hunterlaugh <493418400@qq.com>
2016-10-30 09:38:14 +0000

    add max

---
+++ max.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(void) {
+	int age_zhang=29;
+	int age_yang=26;
+	printf("zhang and yang wo's age is bigger:%d\n",(age_zhang>age_yang)?age_zhang:age_yang);
+	printf("I got the biger people of you!\n");
+}
