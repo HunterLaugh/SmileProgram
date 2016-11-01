900c811bf53f7051a19dbf0093d3a3f70c07cc8e
hunterlaugh <493418400@qq.com>
2016-11-01 03:20:26 +0000

    add input_output product

--- README.md
+++ README.md
@@ -26,3 +26,7 @@
 3. git commit -am "###"      -------以上3步只是本地提交
 4. git remote add origin git@xx.xx.xx.xx:repos/xxx/xxx/xxx.git
 5. git push origin 本地分支:远程分支
+
+## .gitignore文件
+- add  name or *.o  *.a
+- add exe/
---
+++ input_output.c
@@ -0,0 +1,9 @@
+/*Input two numbers,output the product */
+#include<stdio.h>
+void main(void) {
+    int x,y,m; 
+    printf("Please input x and y \n");
+    scanf("%d%d",&x,&y);
+    m=x*y;
+    printf("%d*%d=%d\n",x,y,m);
+}
