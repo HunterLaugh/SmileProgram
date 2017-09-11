83fd8ac8c0c3b85933850988ed40839bd4e5c06b
hunterlaugh <493418400@qq.com>
2017-09-11 12:14:12 +0000

    .vimrc

---
+++ python/ex18.py
@@ -0,0 +1,13 @@
+def ah(ab):
+    print("ab %r" %ab)
+    pass
+def print_one(arg1):
+    print("arg1: %r" %arg1)
+
+ah("ha")
+print_one("Ahha print one")
+print("感动天感动地")
+
+print("天天向上")
+
+print("I like this color")
